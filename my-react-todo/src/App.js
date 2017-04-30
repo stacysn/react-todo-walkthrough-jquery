fe10b5fdf7233c761e665ebb0f390967792f5b59
bgveenstra <bgveenstra@gmail.com>
2017-04-29 18:58:38 -0700

    render Header component in src/App.js

--- my-react-todo/src/App.js
+++ my-react-todo/src/App.js
@@ -1,14 +1,14 @@
 // src/App.js
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>Hello World</h1>
+        <Header />
       </div>
     );
   }
 }
 
-export default App;
+export default App
