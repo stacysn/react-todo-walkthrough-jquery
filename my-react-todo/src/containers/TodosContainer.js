988422fb40a5401ddedf2ddc6d7e49ecbe151625
bgveenstra <b.g.veenstra@gmail.com>
2017-05-01 14:30:40 -0700

    remove Todo import and test todo from TodosContainer

--- my-react-todo/src/containers/TodosContainer.js
+++ my-react-todo/src/containers/TodosContainer.js
@@ -1,7 +1,7 @@
 // src/containers/TodosContainer.js
 import React, {Component} from 'react'
 import TodoModel from '../models/Todo'
-import Todo from '../components/Todo'
+
 
 class TodosContainer extends Component {
   render(){
@@ -12,9 +12,6 @@ class TodosContainer extends Component {
     return (
       <div className='todosContainer'>
         <h2>This is the todos container</h2>
-        <Todo
-          key={"58e3c74c93075e0011489f04"}
-          todo={{"_id":"58e3c74c93075e0011489f04","body":"Take a walk outside","priority":5,"completed":false,"__v":0}}/>
       </div>
     )
   }
