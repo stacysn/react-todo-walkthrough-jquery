f2bad3ec97901817206a10da8b60e0439c599c81
bgveenstra <b.g.veenstra@gmail.com>
2017-05-01 14:31:50 -0700

    import TodoList and render sample TodoList in TodosContainer

--- my-react-todo/src/containers/TodosContainer.js
+++ my-react-todo/src/containers/TodosContainer.js
@@ -1,7 +1,7 @@
 // src/containers/TodosContainer.js
 import React, {Component} from 'react'
 import TodoModel from '../models/Todo'
-
+import TodoList from '../components/TodoList'
 
 class TodosContainer extends Component {
   render(){
@@ -12,6 +12,14 @@ class TodosContainer extends Component {
     return (
       <div className='todosContainer'>
         <h2>This is the todos container</h2>
+        <TodoList
+          todos={
+            [
+              {"_id":"58e3c74c93075e0011489f02","body":"Wash the dishes","priority":4,"completed":false,"__v":0},
+              {"_id":"58e3c74c93075e0011489f07","body":"Update resume with new skills","priority":2,"completed":false,"__v":0},
+              {"_id":"58e3c74c93075e0011489f05","body":"Buy nutritious groceries for the week","priority":2,"completed":false,"__v":0}
+            ]
+          } />
       </div>
     )
   }
