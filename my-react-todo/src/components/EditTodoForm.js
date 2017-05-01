60f04211c3d119dd501271e01e20da18005d96a0
bgveenstra <b.g.veenstra@gmail.com>
2017-05-01 16:19:06 -0700

    add onChange attribute to EditTodoForm input and create simple onInputChange method

--- my-react-todo/src/components/EditTodoForm.js
+++ my-react-todo/src/components/EditTodoForm.js
@@ -2,14 +2,18 @@
 import React, {Component} from 'react'
 
 class EditTodoForm extends Component {
+  onInputChange(event) {
+    console.log('changing a todo!')
+  }
   render(){
     return (
       <div className='editTodoForm'>
         <form>
-          <input
-            placeholder='Write updated todo here...'
-            type='text'
-            value='' />
+        <input
+          onChange={event => this.onInputChange(event)}
+          placeholder='Write updated todo here...'
+          type='text'
+          value='' />
           <button type='submit'>Update Todo!</button>
         </form>
       </div>
