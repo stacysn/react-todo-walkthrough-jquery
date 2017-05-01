6d2186db8174ea0c82e31c35c944d280e24c066b
bgveenstra <b.g.veenstra@gmail.com>
2017-05-01 14:34:31 -0700

    add constructor, componentDidMount, and fetchData methods to TodosContainer

--- my-react-todo/src/containers/TodosContainer.js
+++ my-react-todo/src/containers/TodosContainer.js
@@ -4,18 +4,28 @@ import TodoModel from '../models/Todo'
 import TodoList from '../components/TodoList'
 
 class TodosContainer extends Component {
+  constructor(){
+    super()
+    this.state = {
+      todos: []
+    }
+  }
+  componentDidMount(){
+    this.fetchData()
+  }
+  fetchData(){
+    TodoModel.all().then( (res) => {
+      this.setState ({
+        todos: res.todos
+      })
+    })
+  }
   render(){
     return (
       <div className='todosContainer'>
         <h2>This is the todos container</h2>
         <TodoList
-          todos={
-            [
-              {"_id":"58e3c74c93075e0011489f02","body":"Wash the dishes","priority":4,"completed":false,"__v":0},
-              {"_id":"58e3c74c93075e0011489f07","body":"Update resume with new skills","priority":2,"completed":false,"__v":0},
-              {"_id":"58e3c74c93075e0011489f05","body":"Buy nutritious groceries for the week","priority":2,"completed":false,"__v":0}
-            ]
-          } />
+          todos={this.state.todos} />
       </div>
     )
   }
